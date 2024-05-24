dcc45ba359105a280168e91fc9d206ff6d7e6cc7
m-noma <m-noma@meitec-grp.com>
2024-05-24 10:29:12 +0900

    add practice02

--- GolfPractice/Practice02.py
+++ GolfPractice/Practice02.py
@@ -1,40 +1,27 @@
 import re
 
 
-class Golf:
+class GolfGame:
     def __init__(self, hole_score_array, player_array):
         self.hole_score_array = hole_score_array
         self.player_array = player_array
 
     # 最終ホール計算
-    def total_hole_num(self):
-        result = []
-        for i in self.player_array:
-            if(len(self.hole_score_array) < len(self.player_array[i][1])):
-                result = len(self.hole_score_array)
-            elif(len(self.player_array[i][1]) < len(self.hole_score_array)):
-                result = len(self.player_array[i][1])
-            elif(len(self.player_array[i][1]) == len(self.hole_score_array)):
-                result = len(self.player_array[i][1])
+    def total_hole_num(self, player_score_array):
+        if(len(self.hole_score_array) < len(player_score_array)):
+            result = len(self.hole_score_array)
+        elif(len(player_score_array) < len(self.hole_score_array)):
+            result = len(player_score_array)
+        elif(len(player_score_array) == len(self.hole_score_array)):
+            result = len(player_score_array)
         return result
     
     # スコアを計算し, ポイントの形に変換. (1: パーの値, 2: プレーヤーのスコア)
-    def calc_score(self, total_hole_num):
+    def calc_score(self, total_hole_num, player_score_array):
         total_score = 0
         for i in range(total_hole_num):
-            total_score += int(self.player_score_array[i]) - int(self.hole_score_array[i])
+            total_score += int(player_score_array[i]) - int(self.hole_score_array[i])
         return total_score
-    
-    
-
-
-class GolfPlayer:
-    def __init__(self, player_name, player_score_array):
-        self.player_name = player_name
-        self.player_score_array = player_score_array
-    
-
-    
     
     # スコアの表記方法を改良する 
     def score_conversion(self, calc_num):
@@ -47,63 +34,109 @@ class GolfPlayer:
         else:
             return "calculation errer."
         
-    def play_result(self):
-        return self.calc_score(self.total_hole_num())
-        
-    def show_play_result(self):
-        total_hole_num = self.total_hole_num()
-        print("{}選手: {}ホール終了して{}. \n".format(self.player_name, total_hole_num,
-                                            self.score_conversion(self.calc_score(total_hole_num))))
+    def show_player_result_unit(self, player_num):
+        total_hole_num = self.total_hole_num(self.player_array[player_num].player_score_array)
+        print("{}選手: {}ホール終了して{}.\n".format(self.player_array[player_num].player_name, total_hole_num,
+                                                        self.score_conversion(self.calc_score(total_hole_num, self.player_array[player_num].player_score_array))))
+    
+    def show_play_result_all(self):
+        for i, player in enumerate(self.player_array):
+            total_hole_num = self.total_hole_num(player.player_score_array)
+            print("{}: \n{}選手: {}ホール終了して{}.\n".format(i, player.player_name, total_hole_num,
+                                                        self.score_conversion(self.calc_score(total_hole_num, player.player_score_array))))
+    def show_play_data_unit(self):
+        for i, player in enumerate(self.player_array):
+            print("player{}: {}".format(i, player))
+    
+    def show_result_1vs1(self, player1_num, player2_num):
+        player1 = self.player_array[player1_num]
+        player2 = self.player_array[player2_num]
+        player1_score = self.calc_score(len(self.hole_score_array), player1.player_score_array)
+        player2_score = self.calc_score(len(self.hole_score_array), player2.player_score_array)
+        if(player1_score < player2_score):
+            print("{}の勝利!!\n".format(player1.player_name))
+        elif(player2_score < player1_score):
+            print("{}の勝利!!\n".format(player2.player_name))
+        else:
+            print("引き分け!!")
 
-# 人数の入力を受け付ける
-def input_player_num():
-    input_mun = input("人数を入力: ")
+    
+class GolfPlayer:
+    def __init__(self, player_name, player_score_array):
+        self.player_name = player_name
+        self.player_score_array = player_score_array
 
-# ゴルフプレイの入力を受け付ける
-def input_score():
-    input_str = input("データの入力: ")
-    return input_str
+    def show_player_data(self):
+        print("名前: {},\nスコア: {}\n".format(self.player_name, self.player_score_array))
+
+def divide_name_and_score(input_str, player_num):
+    str_array = input_str.split(",", player_num)
+    return str_array[:player_num], str_array[player_num]
 
 # 引数で与えられた文字列から数字を抜き出す
 def re_base_score(input_str):
     if(input_str == "help"):
         print("""スコアに入力可能な文字は, 「数字(0-9)」, 「カンマ(,)」, 「半角スペース」のみ「カンマ(,)」は数値の区切り文字とする""")
-        return re_base_score(input_score())
-    # elif(input_str == r"[- ,]"):
-    #     print("指定文字以外が含まれています.\n再度入力して下さい.\n")
-    #     return re_base_score(input_score())
+        return re_base_score(input("スコア入力: "))
     else:
         # 空白をまず消し、その後数字を取得する(,区切りで数字を見るため "2 2"を2,2と取りたい場合は.replaceを削除)
         # "3 -5"がはじけない現状だと[3,-5]となってしまうため
         score_array = re.findall(r"-?\d+", input_str.replace(" ", ""))
-        print("入力値: {}".format(score_array))
         # 指定文字以外の配列を作る
         str_array = re.findall(r"[^0-9^,^ ^-]", input_str)
         if(0 < len(re.findall(r"[^0-9^,^ ^-]", input_str))):
-            print("指定文字以外が含まれています.\n再度入力して下さい.\n")
-            return re_base_score(input_score())
-        if(0 < len(re.findall(r'\b\d+\-\d+\b', input_str))):
-            print("計算分を入れないでください.\n再度入力してください.\n")
-            return re_base_score(input_score())
-        if(len(score_array) <= 0):
+            print("スコアに指定文字以外が含まれています.\n再度入力して下さい.\n")
+            return re_base_score(input("スコア入力: "))
+        elif(0 < len(re.findall(r'\b\d+\-\d+\b', input_str))):
+            print("スコアに計算分を入れないでください.\n再度入力してください.\n")
+            return re_base_score(input("スコア入力: "))
+        elif(len(score_array) <= 0):
             # ↓入力が"-"のみの場合「空白です」に入ってしまう...
-            print("空白です.\n再度入力してください.\n")
-            return re_base_score(input_score())
+            print("スコアが空白です.\n再度入力してください.\n")
+            return re_base_score(input("スコア入力: "))
         else:
             for score in score_array:
                 if(int(score) <= 0):
                     print("0以下の値が含まれます.\n再度入力してください.\n")
-                    return re_base_score(input_score())
-            return score_array
-    
+                    return re_base_score(input("スコア入力: "))
+        return score_array
+
+def re_base_array(player_num, constant_array, input_array):
+    score_array = []
+    for i in range(player_num):
+        score = []
+        for j in range(len(constant_array)):
+            # ここにOutOfIndexエラーの際の処理を記述しないとダメ
+            score.append(input_array[i * len(constant_array) + j])
+        score_array.append(score)
+    print("score: {}".format(score_array))
+    return score_array
+
 def main():
-    hole_base_score = [4, 4, 3, 4, 5,
+    constant_array = [4, 4, 3, 4, 5,
                         4, 5, 3, 4, 4,
                         3, 4, 5, 4, 3,
                         4, 5, 4]
-    player_score = input_score()
-    player_score = re_base_score(player_score)
-    GolfPlayer(hole_base_score, "aaaa", player_score).show_play_result()
+    player_list = []
+    input_player_num = int(input("人数を入力: "))
+    input_player_data = input("データ入力: ")
+    player_name_list, input_score = divide_name_and_score(input_player_data, input_player_num)
+    player_score_list = re_base_score(input_score)
+    player_score_list = re_base_array(2, constant_array, player_score_list)
+    for i in range(input_player_num):
+            player_list.append(GolfPlayer(player_name_list[i], player_score_list[i]))
+    # for i, player in enumerate(player_list):
+    #     print("{}: ".format(i+1))
+    #     player.show_player_data()
+    g = GolfGame(constant_array, player_list)
+    g.show_play_result_all()
+    g.show_result_1vs1(0,1)
+    
+
+
+
+    # for i in range():
+
 
 if __name__ == "__main__":
     main()
